d99a89f1a28e83e91f7d04737613bf2586c00108
Benedikt Mandelkow <benedikt.mandelkow@rwth-aachen.de>
2020-10-29 08:55:10 +0100

    add plotting example

--- examples/fullscreen_simple_api.rs
+++ examples/fullscreen_simple_api.rs
@@ -1,12 +1,12 @@
 use imgui::{im_str, Condition};
 use imgui_wgpu::simple_api;
 
-#[derive(Debug)]
 struct State {
     last_frame: std::time::Instant,
     height: f32,
     width: f32,
     highdpi_factor: f64,
+    plotcontext: implot::Context,
 }
 
 fn main() {
@@ -19,11 +19,14 @@ fn main() {
         ..Default::default()
     };
 
+    let plotcontext = implot::Context::create();
+
     let state = State {
         last_frame: std::time::Instant::now(),
         height: 100.0,
         width: 100.0,
         highdpi_factor: 2.0,
+        plotcontext,
     };
 
     imgui_wgpu::simple_api::run(config, state, |ui, state| {
@@ -43,6 +46,23 @@ fn main() {
             .menu_bar(true)
             .build(&ui, || {
                 ui.text(im_str!("Hello world!"));
+
+                // begin implot
+                let plot_ui = &state.plotcontext.get_plot_ui();
+
+                let content_width = ui.window_content_region_width();
+                implot::Plot::new("Simple line plot")
+                    // The size call could also be omitted, though the defaults don't consider window
+                    // width, which is why we're not doing so here.
+                    .size(content_width, 300.0)
+                    .build(plot_ui, || {
+                        // If this is called outside a plot build callback, the program will panic.
+                        let x_positions = vec![0.1, 0.9];
+                        let y_positions = vec![0.1, 0.9];
+                        implot::PlotLine::new("legend label").plot(&x_positions, &y_positions);
+                    });
+
+                // end implot
             });
 
         state.last_frame = now;
